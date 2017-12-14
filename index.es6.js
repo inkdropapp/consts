4f5aedbe42df03f1e8d3f4be9de54e8f47bc13f5
Takuya Matsuyama <nora@odoruinu.net>
2017-12-14 17:50:48 +0900

    Use babel

---
+++ index.es6.js
@@ -0,0 +1,45 @@
+export const appName = 'Inkdrop'
+export const contactMaddr = 'contact@inkdrop.info'
+export const webUrl = 'https://www.inkdrop.info/'
+export const docUrl = 'http://doc.inkdrop.info/'
+export const forumUrl = 'https://forum.inkdrop.info/'
+export const creditsUrl = 'https://github.com/inkdropapp/licenses/blob/master/README.md'
+export const accountStatus = {
+  ACTIVE: 'active',
+  DEACTIVATED: 'deactivated',
+  DELETED: 'deleted'
+}
+export const accountType = {
+  TRIAL: 'trial',
+  BASIC: 'basic'
+}
+export const planType = {
+  basicMonthly: 'basic_usd_m',
+  basicYearly: 'basic_usd_y',
+  basicMonthlyJPY: 'basic_m',
+  basicYearlyJPY: 'basic_y'
+}
+export const sessionStatus = {
+  ACTIVE: 'active',
+  NOT_LOGGED_IN: 'notLoggedIn',
+  UNAUTHORIZED: 'unauthorized',
+  TRIAL_EXPIRED: 'trialExpired',
+  SESSION_EXPIRED: 'sessionExpired',
+  OLD_VERSION: 'oldVersion'
+}
+export const trashBookId = 'trash'
+export const noteStatus = {
+  NONE: 'none',
+  ACTIVE: 'active',
+  ON_HOLD: 'onHold',
+  COMPLETED: 'completed',
+  DROPPED: 'dropped'
+}
+export const noteShare = {
+  PRIVATE: 'private',
+  PUBLIC: 'public'
+}
+export const tagColors = [ 'default', 'red', 'orange', 'yellow', 'olive', 'green', 'teal', 'blue', 'violet', 'purple', 'pink', 'brown', 'grey', 'black' ]
+export const supportedImageFileTypes = [ 'image/png', 'image/jpeg', 'image/jpg', 'image/svg', 'image/gif' ]
+export const maxAttachmentFileSize = 10 * 1024 * 1024
+export const taxPercentage = 8
