77697f9c9a2e4bcc2776338c55085d605b590d07
Takuya Matsuyama <nora@odoruinu.net>
2017-12-18 02:31:25 +0900

    Support flowtype

--- index.es6.js
+++ index.es6.js
@@ -1,26 +1,54 @@
-export const appName = 'Inkdrop'
-export const contactMaddr = 'contact@inkdrop.info'
-export const webUrl = 'https://www.inkdrop.info/'
-export const appUrl = 'https://app.inkdrop.info/'
-export const docUrl = 'http://doc.inkdrop.info/'
-export const forumUrl = 'https://forum.inkdrop.info/'
-export const creditsUrl = 'https://github.com/inkdropapp/licenses/blob/master/README.md'
-export const accountStatus = {
+// @flow
+export const appName: string = 'Inkdrop'
+export const contactMaddr: string = 'contact@inkdrop.info'
+export const webUrl: string = 'https://www.inkdrop.info/'
+export const appUrl: string = 'https://app.inkdrop.info/'
+export const docUrl: string = 'http://doc.inkdrop.info/'
+export const forumUrl: string = 'https://forum.inkdrop.info/'
+export const creditsUrl: string = 'https://github.com/inkdropapp/licenses/blob/master/README.md'
+export type AccountStatus = 'active' | 'deactivated' | 'deleted'
+export type AccountStatusConstType = {
+  ACTIVE: string,
+  DEACTIVATED: string,
+  DELETED: string
+}
+export const accountStatus: AccountStatusConstType = {
   ACTIVE: 'active',
   DEACTIVATED: 'deactivated',
   DELETED: 'deleted'
 }
-export const accountType = {
+export type AccountType = 'trial' | 'basic'
+export type AccountTypeConstType = {
+  TRIAL: string,
+  BASIC: string
+}
+export const accountType: AccountTypeConstType = {
   TRIAL: 'trial',
   BASIC: 'basic'
 }
-export const planType = {
+export type PlanType = 'basic_usd_m' | 'basic_usd_y' | 'basic_m' | 'basic_y'
+export type PlanTypeConstType = {
+  basicMonthly: string,
+  basicYearly: string,
+  basicMonthlyJPY: string,
+  basicYearlyJPY: string
+}
+export const planType: PlanTypeConstType = {
   basicMonthly: 'basic_usd_m',
   basicYearly: 'basic_usd_y',
   basicMonthlyJPY: 'basic_m',
   basicYearlyJPY: 'basic_y'
 }
-export const sessionStatus = {
+export type SessionStatus = 'active' | 'notLoggedIn' | 'unauthorized' | 'trialExpired' | 'sessionExpired' | 'oldVersion'
+export type SessionStatusConstType = {
+  ACTIVE: string,
+  NOT_LOGGED_IN: string,
+  UNAUTHORIZED: string,
+  TRIAL_EXPIRED: string,
+  SESSION_EXPIRED: string,
+  OLD_VERSION: string
+}
+export const sessionStatus: SessionStatusConstType = {
   ACTIVE: 'active',
   NOT_LOGGED_IN: 'notLoggedIn',
   UNAUTHORIZED: 'unauthorized',
@@ -28,7 +56,7 @@ export const sessionStatus = {
   SESSION_EXPIRED: 'sessionExpired',
   OLD_VERSION: 'oldVersion'
 }
-export const trashBookId = 'trash'
+export const trashBookId: string = 'trash'
 export const noteStatus = {
   NONE: 'none',
   ACTIVE: 'active',
