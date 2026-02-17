691f1ce55e7a968da42fb54c21898fa25d1b89ce
Takuya Matsuyama <hi@craftz.dog>
2026-02-17 11:39:24 +0900

    chore(dev): bump up toolchain

---
+++ eslint.config.mjs
@@ -0,0 +1,26 @@
+import eslint from '@eslint/js'
+import tseslint from 'typescript-eslint'
+import eslintConfigPrettier from 'eslint-config-prettier'
+
+export default tseslint.config(
+  eslint.configs.recommended,
+  ...tseslint.configs.recommended,
+  eslintConfigPrettier,
+  {
+    files: ['src/**/*.ts'],
+    rules: {
+      'no-useless-escape': 'off',
+      '@typescript-eslint/no-explicit-any': 'off',
+      '@typescript-eslint/no-unused-vars': [
+        'error',
+        {
+          argsIgnorePattern: '^_',
+          varsIgnorePattern: '^_'
+        }
+      ]
+    }
+  },
+  {
+    ignores: ['lib/**', 'node_modules/**']
+  }
+)
